793ccec9e6b5491d4ad49b9155006b862ac7eb29
mardon <mar.don@seznam.cz>
2022-10-26 12:32:37 +0200

    fix BookCategoryRepository

--- src/Repository/BookCategoryRepository.php
+++ src/Repository/BookCategoryRepository.php
@@ -9,10 +9,10 @@ use Doctrine\Persistence\ManagerRegistry;
 /**
  * @extends ServiceEntityRepository<Book>
  *
- * @method Book|null find($id, $lockMode = null, $lockVersion = null)
- * @method Book|null findOneBy(array $criteria, array $orderBy = null)
- * @method Book[]    findAll()
- * @method Book[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method BookCategory|null find($id, $lockMode = null, $lockVersion = null)
+ * @method BookCategory|null findOneBy(array $criteria, array $orderBy = null)
+ * @method BookCategory[]    findAll()
+ * @method BookCategory[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class BookCategoryRepository extends ServiceEntityRepository
 {
